3ed6517869b288dc85620d89fdf4a7b83a1f8919
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-20 21:11:18 -0300

    Criando metodos

--- classes/reservas.class.php
+++ classes/reservas.class.php
@@ -20,5 +20,60 @@ class Reservas {
 		return $array;
 	}
 
+	public function verificarDisponibilidade($carro, $data_inicio, $data_fim) {
+		$sql = $this->pdo->prepare("SELECT * FROM reservas WHERE id_carro = :carro 
+		AND (data_fim > :data_inicio AND data_inicio < :data_fim)");
+		$sql->bindValue(":carro", $carro);
+		$sql->bindValue(":data_inicio", $data_inicio);
+		$sql->bindValue(":data_fim", $data_fim);
+		$sql->execute();
+
+		if($sql->rowCount() > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public function reservar($carro, $data_inicio, $data_fim, $pessoa) {
+		$sql = $this->pdo->prepare("INSERT INTO reservas (id_carro, data_inicio, data_fim, pessoa) VALUES (:carro, :data_inicio, :data_fim, :pessoa)");
+		$sql->bindValue(":carro", $carro);
+		$sql->bindValue(":data_inicio", $data_inicio);
+		$sql->bindValue(":data_fim", $data_fim);
+		$sql->bindValue(":pessoa", $pessoa);
+		$sql->execute();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
